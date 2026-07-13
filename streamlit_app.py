7ea62cfac80cf372d76b9a91765b37f1035cfa74
sachinekbote <82711884+sachinekbote@users.noreply.github.com>
2026-07-13 18:13:13 -0500

    Change data_frame to dataframe for consistency
    removed error for dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ elif ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
-sf_df = st.data_frame(data=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
