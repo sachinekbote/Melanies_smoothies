701fe7214c44863e4b32f37cfe35c60a03a3283f
sachinekbote <82711884+sachinekbote@users.noreply.github.com>
2026-07-13 19:09:49 -0500

    Fix API request URL formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ elif ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen) 
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{SEARCH_ON})  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{SEARCH_ON}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
